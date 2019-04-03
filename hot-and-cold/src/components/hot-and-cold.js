b6cfe51fa9600f02e80566c96270b66e64ccd790
Race Briggs <race.briggs@gmail.com>
2019-04-03 11:59:05 -0500

    started final compilation component

---
+++ hot-and-cold/src/components/hot-and-cold.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from './header.js';
+import GuessSection from './guess-section.js';
+import StatusSection from './status-section.js';
+
+export default class Game extends React.Component {
+	
+
+	
+}
